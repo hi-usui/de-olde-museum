7e800ed0be08834b94d187627195fd888dcb0887
Hiroshi Usui <usui@berkeley.edu>
2021-08-06 18:14:27 -0700

    add skeleton of files

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
-import ScreenOne from "components/ScreenOne";
+import Landing from "components/Landing";
+import Phone from "components/Phone";
+import Screen1 from "components/Screen1";
+import Screen2 from "components/Screen2";
+import Screen3 from "components/Screen3";
 import React, { Component } from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -12,7 +16,11 @@ class App extends Component {
         <Router>
           <div className="App">
             <Switch>
-              <Route exact path="/" component={ScreenOne} />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/screen1" component={Screen1} />
+              <Route exact path="/screen2" component={Screen2} />
+              <Route exact path="/screen3" component={Screen3} />
+              <Route exact path="/phone" component={Phone} />
             </Switch>
           </div>
         </Router>
